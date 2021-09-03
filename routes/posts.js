f822ce401a2c75d7392ec91d4a9049b2731bc26d
jadick <adickja@gmail.com>
2021-09-03 16:06:28 -0400

    created routes and controllers for update and delete CRUD functionality. added server.js functionality.

--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/posts', controllers.getPosts)
 router.get('/posts/:id', controllers.getPost)
 router.post('/posts', controllers.createPost)
+router.put('/posts/:id', controllers.updatePost)
+router.delete('/posts/:id', controllers.deletePost)
 
 export default router
