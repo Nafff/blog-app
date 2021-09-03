345eba5bf2e87b61090da1af9c79f30c7a6a5d6b
jadick <adickja@gmail.com>
2021-09-03 15:35:23 -0400

    added routes and controllers for getPost by ID and createPost

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,3 +8,28 @@ export const getPosts = async (req, res) => {
     res.status(500).send(error.message)
   }
 }
+
+export const getPost = async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id)
+    if (post) {
+      return res.json(post)
+    }
+    res.status(404).json({ message: 'Post not found!' })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ error: error.message })
+  }
+}
+
+export const createPost = async (req, res) => {
+  try {
+    const post = new Post(req.body)
+    await post.save()
+    res.status(201).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message })
+  }
+}
