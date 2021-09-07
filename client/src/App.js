545e9fcdd84ce8268bc6f66e8a3c2d3e7cfb9c52
jadick <adickja@gmail.com>
2021-09-07 16:57:53 -0400

    set up routes and base component functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
+import Home from './screens/Home/Home'
+import Posts from './screens/Posts/Posts'
+import PostCreate from './screens/PostCreate/PostCreate'
+import PostDetails from './screens/PostDetails/PostDetails'
+import PostEdit from './screens/PostEdit/PostEdit'
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Posts} />
+        <Route path="/add-posts" component={PostCreate} />
+        <Route exact path="/posts/:id" component={PostDetails} />
+        <Route exact path="/posts/:id/edit" component={PostEdit} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -0,0 +1,7 @@
+function PostCreate() {
+  return (
+    <h1>Create kool posts</h1>
+  )
+}
+
+export default PostCreate;
