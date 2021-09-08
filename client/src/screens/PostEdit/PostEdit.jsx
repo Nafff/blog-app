3144fba20e91d78491eb570f3bded322c8085587
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-09-08 11:59:39 -0700

    more taco ipsum

--- client/src/screens/PostEdit/PostEdit.jsx
+++ client/src/screens/PostEdit/PostEdit.jsx
@@ -1,6 +1,10 @@
 function PostEdit() {
   return (
-    <h1>new text</h1>
+    <>
+      <h1>new text</h1>
+      <h2>Taco Gang</h2>
+      <h2>I wish I was full of tacos instead of emotions</h2>
+    </>
   )
 }
 
