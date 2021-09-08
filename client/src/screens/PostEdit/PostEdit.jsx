7192f54377c4940d5d34be37064a2b918a581134
guess01325 <guess01325@gmail.com>
2021-09-08 14:59:28 -0400

    update edit

--- client/src/screens/PostEdit/PostEdit.jsx
+++ client/src/screens/PostEdit/PostEdit.jsx
@@ -1,6 +1,13 @@
 function PostEdit() {
   return (
-    <h1>new text</h1>
+    <div>
+      <h1>This is my code</h1>
+      <form>
+        <img src="https://media.discordapp.net/attachments/866850568559788035/885236017102209094/Rauls_background.png?width=1173&height=660" alt="" />
+        <input>locaton</input>
+      </form>
+
+    </div>
   )
 }
 
