1c0dcd7913bf14e46d2e14353119b023b4c8bded
jadick <adickja@gmail.com>
2021-09-08 14:58:30 -0400

    merge conflict testing

--- client/src/screens/PostEdit/PostEdit.jsx
+++ client/src/screens/PostEdit/PostEdit.jsx
@@ -1,6 +1,16 @@
 function PostEdit() {
+  const tacoTypes = ['Carne Asada', 'Shrimp Tacos', 'Barbacoa', 'Tinga Tacos']
   return (
-    <h1>new text</h1>
+    <>
+    <h1>TacoTown</h1>
+      <h2>Population: 15 Taco Bells</h2>
+      <p>I’ve been following that taco truck around all day. Tacos, again? This will be 5 times this week and it’s only Tuesday. You see, the refried beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell. 50 cent tacos! I’ll take 30. I’d have to say, those tacos are on fleek. How bout a gosh darn quesadilla? Shrimp tacos are tasty tacos! Let’s do a beef and a chicken, and one with both. Tacos, tacos, tacos.</p>
+      {tacoTypes.map((taco) => {
+        return (
+          <h5>{taco}</h5>
+        )
+      })}
+    </>
   )
 }
 
