c6568495d81bdbedd1a6b0338b6c34f1d47f4696
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-09-08 08:52:00 -0700

    added basic detail page structure using props

--- client/src/screens/PostDetails/PostDetails.jsx
+++ client/src/screens/PostDetails/PostDetails.jsx
@@ -1,6 +1,19 @@
-function PostDetails() {
+function PostDetails(props) {
+
+  const {title, author, details, relevantLinks} = props.posts
+console.log(props)
+
+  if (!props.posts) {
+    return <h1>No posts found!</h1>
+  }
+
   return (
-    <h1>This is a kool detail</h1>
+    <>
+    <h1>Title: {title}</h1>
+    <h1>Aurthor: {author}</h1>
+    <h1>Details: {details}</h1>
+    <h1>Link: {relevantLinks}</h1>
+  </>
   )
 }
 
