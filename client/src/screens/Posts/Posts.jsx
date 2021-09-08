f4459c0185767750e931bcffa11248115132e134
jadick <adickja@gmail.com>
2021-09-08 12:01:30 -0400

    created component Post and screen Post functionality

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,22 +1,31 @@
-import './Posts.css'
-import { useState, useEffect } from 'react'
+import "./Posts.css";
+import { useState, useEffect } from "react";
+import Post from ".../components/Post/Post";
 
 function Posts() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const getPosts = async () => {
-      const 
-    }
-  })
+      const allPosts = await getPosts();
+      setPosts(allPosts);
+    };
+  });
 
   return (
     <Layout>
-      <div className='products'>
-
+      <div className="products">
+        {posts.map((post) => (
+          <Post
+            title={post.title}
+            author={post.author}
+            details={post.details}
+            relevantLinks={post.relevantLinks}
+          />
+        ))}
       </div>
     </Layout>
-  )
+  );
 }
 
 export default Posts;
